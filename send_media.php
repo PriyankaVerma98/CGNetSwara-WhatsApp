a68130e2a495f2fc51a0f64fbb91e311516c2775
Priyanka Verma <39693183+PriyankaVerma98@users.noreply.github.com>
2021-05-04 16:31:06 +0530

    http response with media links/status
    Receives http request from IMI and responds back in http body with media links for published stories and status codes of unpublished stories.

---
+++ send_media.php
@@ -0,0 +1,101 @@
+<?php
+echo "Hello World\n" ;
+echo "\n".$_SERVER['SERVER_NAME'];
+echo "\n".$_SERVER['SERVER_ADDR'];
+echo "\n".$_SERVER['GATEWAY_INTERFACE'];
+echo "\n".$_SERVER['SERVER_PROTOCOL'];
+
+echo "\n  <br/> here are http deyails:";  // https://www.php.net/manual/en/reserved.variables.server.php 
+// // webs server creates these entries
+echo "\n <br/>HTTP_CONNECTION: ".$_SERVER['HTTP_CONNECTION'];
+echo "\n <br/>HTTP_ACCEPT: ".$_SERVER['HTTP_ACCEPT'];
+echo "\n <br/>REQUEST_METHOD: ".$_SERVER['REQUEST_METHOD'];
+echo "\n <br/>QUERY_STRING: ".$_SERVER['QUERY_STRING'];
+echo " <br/>HTTP_HOST: ".$_SERVER['HTTP_HOST'];
+echo " <br/>REQUEST_URI: ".$_SERVER['REQUEST_URI'];
+echo "<br/>";
+
+//try to get all the headers  // WORKS !!! browser and imi are 2 clients both have diiferent headers
+// // https://www.geeksforgeeks.org/how-to-read-any-request-header-in-php/
+echo "\n getting all headers <br/>";
+$header = apache_request_headers();
+foreach ($header as $headers => $value) {
+	echo "$headers: $value <br />\n";
+}
+
+$da = file_get_contents('php://input');
+// echo "\n <br/>I received http body: " ;
+$dat = explode(",",$da);
+$data = explode(":",$dat[2]);
+$trimmed = substr($data[1], 0, strlen($data[1])-3);
+// echo "\n data[1]= ".$trimmed;
+$no = substr($trimmed, -10);
+// echo "\n <br/>I received http data:";
+// echo "\n user no= ".$no;
+
+
+//include database stuff and functions
+include "loudblog/custom/config.php";
+include "loudblog/inc/database/adodb.inc.php";
+include "loudblog/inc/connect.php";
+include "loudblog/inc/functions.php";
+
+//create some important globals
+if (!isset($db['host'])) {
+    die("<br /><br />Cannot find a valid configuration file! <a href=\"install.php\">Install Loudblog now!</a>");
+}
+
+$GLOBALS['prefix']     = $db['pref'];
+$GLOBALS['path']       = $lb_path;
+$GLOBALS['audiopath']  = $lb_path . "/audio/";
+$GLOBALS['uploadpath'] = $lb_path . "/upload/";
+
+
+$status_values = array(1  => "DRAFT", 2 => "FINISHED" , 3 => "ON AIR", 4 => "STR", 5 =>"ES" , 6 => "MODENC", 7 =>"NTR"  );
+// //entries for Videos
+$dosql = "SELECT * FROM ".$GLOBALS['prefix']."lb_postings WHERE user = '". $no ."' AND video_file IS NOT NULL order by id DESC";
+$result = $GLOBALS['lbdata']->GetArray($dosql);
+
+$rows_total = count($result);
+$k = 3 ;
+$rows = min($k, $rows_total);
+// echo "\n".$rows."\n";
+for ($row = 0; $row < $rows; $row++) {
+	$entry_num=$row+1 ;
+	echo $entry_num.") ";
+	if(is_null($result[$row]['video_link']) || $result[$row]['video_link']== "" ){
+		echo "Video not published!"." Received it on our server on: ".$result[$row]['posted'].". It has status= ".$status_values[$result[$row]['status']]." Contact team with reference id: ".$result[$row]['id'];
+    	echo "\n";
+	}
+	else{
+		echo "Your video has been published at: ".$result[$row]['video_link'];
+    	echo "\n";
+	}
+}
+if($rows== 0)
+	echo "no videos received from this number\n";
+
+// // Audio entries
+$dosql = "SELECT * FROM ".$GLOBALS['prefix']."lb_postings WHERE user = '". $no ."' AND video_file IS NULL order by id DESC";
+$result = $GLOBALS['lbdata']->GetArray($dosql);
+$rows_total = count($result);
+$k = 3 ;
+$rows = min($k, $rows_total);
+echo "\n \nAudio entries: \n" ;
+for ($row = 0; $row < $rows; $row++) {
+	$entry_num=$row+1 ;
+	echo $entry_num.") ";
+	if($result[$row]['status'] != 3){
+		echo "your audio is not published. It has status= ".$status_values[$result[$row]['status']];
+    	echo "\n";
+	}
+	else{
+		$audio_id= $result[$row]['id'];
+		echo "Your audio has been published at: http://cgnetswara.org/index.php?id=".$audio_id;
+    	echo "\n";
+	}
+}
+if($rows== 0)
+	echo "no audios received from this number\n";
+
+?>
